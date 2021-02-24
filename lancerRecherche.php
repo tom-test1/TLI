4033cd2fb2025052fb5156f124105aa267dce781
tom <tommy-80@hotmail.fr>
2021-02-24 18:02:48 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- lancerRecherche.php
+++ lancerRecherche.php
@@ -1,4 +1,4 @@
-<?php>
+<?php
 $conn = new PDO('pgsql:host=localhost;port=5432;dbname=acudb','postgres-tli','tli');
 
 print("test recherche");
--- lancerScript.php
+++ lancerScript.php
@@ -1,6 +1,18 @@
-<?php>
+<?php
 $conn = new PDO('pgsql:host=localhost;port=5432;dbname=acudb','postgres-tli','tli');
 
 print("cest un test stp");
+
+
+$sth = $conn->prepare('SELECT * FROM meridien');
+
+$sth-> execute();
+
+$result= $sth->fetchAll();
+
+
+print_r($result);
+
+
 
 /* include sur une page html pour recuperer les données*/
