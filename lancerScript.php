97d2acc234f0e9775c87b3e36fd2b4af62d30e91
tli <Tom>
2021-02-24 16:58:14 +0100

    icone partout

--- lancerScript.php
+++ lancerScript.php
@@ -1,6 +1,6 @@
 <?php>
 $conn = new PDO('pgsql:host=localhost;port=5432;dbname=acudb','postgres-tli','tli');
 
-console.log("cest un test stp")
+print("cest un test stp");
 
 /* include sur une page html pour recuperer les données*/
