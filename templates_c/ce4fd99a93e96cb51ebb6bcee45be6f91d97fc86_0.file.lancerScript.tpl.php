5ebf954449e85dd43791d2274b3661a8d8e3f198
clementcornu <clement.cornu@cpe.fr>
2021-03-18 12:13:30 +0100

    travail page2.tpl

--- templates_c/ce4fd99a93e96cb51ebb6bcee45be6f91d97fc86_0.file.lancerScript.tpl.php
+++ templates_c/ce4fd99a93e96cb51ebb6bcee45be6f91d97fc86_0.file.lancerScript.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-18 10:57:18
+/* Smarty version 3.1.39, created on 2021-03-18 11:59:10
   from '/var/www/html/TLI/templates/lancerScript.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605323fe637297_66957382',
+  'unifunc' => 'content_6053327e37f251_09402854',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ce4fd99a93e96cb51ebb6bcee45be6f91d97fc86' => 
     array (
       0 => '/var/www/html/TLI/templates/lancerScript.tpl',
-      1 => 1616061111,
+      1 => 1616065148,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_605323fe637297_66957382 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6053327e37f251_09402854 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 
 <html lang="fr-FR">
@@ -121,8 +121,74 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     
   </table>
 </div>
-</body>
 
+<form action="./lancerRecherche.php">
+
+
+  </br>
+  <p> Remplissez au moins un des champs suivants :</p>
+  <br>
+
+  <p>Mots-clefs :
+      <input type="text" placeholder="Ex : Poumon" id="mot_clef" name="mot_clef">
+  </p>
+
+  <p>Symptômes :
+      <input type="text" placeholder="Ex : Baîllements" id="symptome" name="symptome">
+  </p>
+
+  <p>Pathologie :
+  <input type="text" placeholder="Ex : voie luo de la rate vide" id="patho" name="patho">
+  </p>
+
+  <p>Méridiens :
+      <input type="text" placeholder="Ex : Chong Mai" id="meridien" name="meridien">
+  </p>
+
+  </br>
+  <p> Filtres :</p>
+  <br>
+
+  <input type="checkbox" id="keywords_idk" name="keywords_idk" value="true">
+  <label for="keywords_idk"> idk </label><br>
+
+  <input type="checkbox" id="keywords_name" name="keywords_name" value="true" checked>
+  <label for="keywords_name"> mot-clef </label><br>
+
+  <input type="checkbox" id="keySympt_ids" name="keySympt_ids" value="true">
+  <label for="keySympt_ids"> ids </label><br>
+
+  <input type="checkbox" id="symptome_desc" name="symptome_desc" value="true" checked>
+  <label for="symptome_desc"> symptome </label><br>
+
+  <input type="checkbox" id="symptPatho_idp" name="symptPatho_idp" value="true">
+  <label for="symptPatho_idp"> idp </label><br>
+
+  <input type="checkbox" id="symptPatho_aggr" name="symptPatho_aggr" value="true">
+  <label for="symptPatho_aggr"> aggr </label><br>
+
+  <input type="checkbox" id="patho_mer" name="patho_mer" value="true">
+  <label for="patho_mer"> mer </label><br>
+
+  <input type="checkbox" id="patho_type" name="patho_type" value="true">
+  <label for="patho_type"> type patho </label><br>
+
+  <input type="checkbox" id="patho_desc" name="patho_desc" value="true" checked>
+  <label for="patho_desc"> desc patho </label><br>
+
+  <input type="checkbox" id="meridien_nom" name="meridien_nom" value="true" checked>
+  <label for="meridien_nom"> nom meridien</label><br>
+  
+  <input type="checkbox" id="meridien_element" name="meridien_element" value="true">
+  <label for="meridien_element"> element meridien </label><br>
+
+  <input type="checkbox" id="meridien_yin" name="meridien_yin" value="true">
+  <label for="meridien_yin"> yin meridien </label><br>
+  <br>
+
+  <input type="submit" value="Rechercher">
+
+</form>
 </body>
 </html><?php }
 }
